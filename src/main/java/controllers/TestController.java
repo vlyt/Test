b2d47c60288bddf0bcbcbc325367a98f492848c4
Vitaliy Lytvyn <vitaliy.lytvyn@openexc.com>
2021-06-21 18:53:11 +0300

    changed names

--- src/main/java/controllers/TestController.java
+++ src/main/java/controllers/TestController.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class TestController1 {
+public class TestController2 {
 
     @GetMapping("/do")
     public ResponseEntity test(){
-        String t = "<html>test123</html>";
+        String t = "<html>test2</html>";
         return new ResponseEntity<>(t, HttpStatus.OK);
     }
 }
